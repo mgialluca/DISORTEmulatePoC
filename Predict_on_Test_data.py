3995e1e1646a6ecf3a7eec7f7bd5b2d0700ccce3
Megan Gialluca <gialluca@uw.edu>
2025-08-20 09:38:20 -0700

    Fixing array indexing for differently sized datasets

--- Predict_on_Test_data.py
+++ Predict_on_Test_data.py
@@ -9,10 +9,16 @@ model = keras.models.load_model('Version1.keras')
 hf = h5py.File('/gscratch/vsm/gialluca/PostDocPropose/DISORT_Testing_Data.h5', 'r')
 x_test = np.array(hf['IN'])
 
+# Determine how big the datasets should be:
+inds = []
+arr = np.array(hf['IN'])
+for i in range(len(arr)-1):
+    if arr[i][0] > arr[i+1][0]:
+        inds.append(i+1)
+
 # Run Prediction
 st = dt.now() # to determine runtime
-# 3 datasets, each with 298620 wavelength points
-predictions = model.predict(x_test[:298620])
+predictions = model.predict(x_test[:inds[0]])
 np.save('Predictions_Test1.npy', predictions)
 fn = dt.now()
 
@@ -20,8 +26,7 @@ print('Time for emulation 1: ')
 print(fn-st)
 
 st = dt.now() # to determine runtime
-# 3 datasets, each with 298620 wavelength points
-predictions = model.predict(x_test[298620:298620*2])
+predictions = model.predict(x_test[inds[0]:inds[1]])
 np.save('Predictions_Test2.npy', predictions)
 fn = dt.now()
 
@@ -29,11 +34,17 @@ print('Time for emulation 2: ')
 print(fn-st)
 
 st = dt.now() # to determine runtime
-# 3 datasets, each with 298620 wavelength points
-predictions = model.predict(x_test[298620*2:298620*3])
+predictions = model.predict(x_test[inds[1]:])
 np.save('Predictions_Test3.npy', predictions)
 fn = dt.now()
 
 print('Time for emulation 3: ')
 print(fn-st)
 
+
+# Running on hyak for interactive node yielded runtimes of:
+# 9.3 sec, 8.5 sec, 8.7 sec; avg of 8.8 sec
+
+# Each DISORT run took 54 min 18 sec
+
+# Emulation yielded over a 370x speed up
